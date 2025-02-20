5f17c29941cdcfd9f53920e0b1fd656e91b344f7
yoka438 <yoka.10438@mhs.amikomsolo.ac.id>
2025-02-20 21:33:55 +0700

    Menambahkan File yoka.py

---
+++ team/yoka.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, Nama Saya", nama)
+    print("NIM : ", nim)
+    print("Peran saya yaitu : ", peran)
+
+if __name__ == "__main__":
+    nama = "Yoka Hobi Amaliawati"
+    nim = "2213010438"
+    peran = "Fitur Tambah Data, Edit Data, Hapus Data"
+    perkenalan(nama, nim, peran)
