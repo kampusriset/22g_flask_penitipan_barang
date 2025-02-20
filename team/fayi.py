ebb90f78d597a1ca6628e44d0035e2b5206985ff
fayi416 <fayi.10416@mhs.amikomsolo.ac.id>
2025-02-20 21:49:00 +0700

    Menambahkan File fayi.py

---
+++ team/fayi.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, Nama Saya", nama)
+    print("NIM :", nim)
+    print("Peran saya yaitu : ", peran)
+    
+if __name__ == "__main__":
+    nama = "Fayi Agnia Limara"
+    nim  = "2213010416"
+    peran = "Login, Logout, Search"
+    perkenalan(nama, nim, peran)
